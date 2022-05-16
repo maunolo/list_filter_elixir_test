58ccf13b4f122a5f253c55fa23d1bf6fb46c3cae
Mauricio Lopes <mauricionlopes.dev@gmail.com>
2022-05-16 10:44:18 -0300

    using pipe operator instead of case

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,14 +1,17 @@
 require Integer
 
 defmodule ListFilter do
-  def call(list), do: count_odd_integers(list)
-
-  defp count_odd_integers(list) do
-    Enum.count(list, fn string ->
-      case Integer.parse(string) do
-        {int, _rest} -> if Integer.is_odd(int), do: true, else: false
-        :error -> false
-      end
-    end)
+  def call(list) do
+    list
+    |> Enum.count(fn string -> is_string_odd_integer(string) end)
   end
+
+  defp is_string_odd_integer(string) do
+    string
+    |> Integer.parse()
+    |> handle_string()
+  end
+
+  defp handle_string({int, _rest}), do: Integer.is_odd(int)
+  defp handle_string(:error), do: false
 end
